b5ab0bcaa33ea04de27f01c11bfb949941fb6e29
MishaGaer <gaer1997@yandex.ru>
2022-08-07 16:04:02 +0200

    Add_InputControl

--- Program/Program.cs
+++ Program/Program.cs
@@ -5,3 +5,31 @@
 Первоначальный массив можно вводить с клавиатуры,
 либо задать на старте выполнения алгоритма.
 */
+//Контроль ввода
+int InputControl(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            if (number > 0)
+            {
+                return number;
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели значение меньше допустимого. Пожалуйста введите корректное значение.");
+            }
+        }
+        else
+        {
+            Console.Clear();
+            Console.WriteLine("Вы ввели не число. Пожалуйста введите корректное значение.");
+        }
+    }
+}
+
+//Основной код
+int arraySize = InputControl("Введите размер массива.");
+Console.Clear();
